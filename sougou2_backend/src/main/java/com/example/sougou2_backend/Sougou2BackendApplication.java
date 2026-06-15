c9962c4333803f6fef580c9273f729ca32759d3a
RJC\koki_shinzato <koki_shinzato@r-jc.jp>
2026-06-15 11:38:42 +0900

    環境構築完了

---
+++ sougou2_backend/src/main/java/com/example/sougou2_backend/Sougou2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.sougou2_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sougou2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sougou2BackendApplication.class, args);
+	}
+
+}
---
+++ sougou2_backend/src/test/java/com/example/sougou2_backend/Sougou2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sougou2_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sougou2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
